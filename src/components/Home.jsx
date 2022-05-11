d1e506611b9d95d7e9a7cad76408cd0a42c893fd
nyaribari <anyaribari@gmail.com>
2022-05-11 08:53:37 +0300

    React-project frontent

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Home</h1>
+            <p>
+              Join skaes to say and share your skites as news, comments and memes.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home
