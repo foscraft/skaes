8172a51c25dcedac083b27de7813fbb424adb842
nyaribari <anyaribari@gmail.com>
2022-05-11 09:03:18 +0300

    Chilanka font updates

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import {NavLink} from "react-router-dom"
 
 const Navigation = () => {
-  return (<div className="navigation">
+  return (
+    <div className="navigation">
   <nav className="navbar navbar-expand navbar-dark bg-dark">
     <div className="container">
       <NavLink className="navbar-brand" to="/">
