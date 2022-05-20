934512341c12d2a68fd32b9c64ebea5778401b4f
nyaribari <anyaribari@gmail.com>
2022-05-20 10:13:18 +0300

    Adding contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,16 +15,7 @@ function Contact() {
           <div class="col-lg-5">
             <h1 class="font-weight-light">Contact</h1>
             <p>
-            As a consequence of the fact that the most famous poems tend to be the older ones,
-             they also often have distinct rhyme schemes threaded throughout the verses. 
-             While I personally do think rhyming poems are generally better, 
-             and that partially also accounts for their fame, I recognise how subjective this adjective is.
-              Moreover, non-rhyming poems have played a significant role in literature during the 
-              last century in particular. In fact, the influence of poems such as Ginsberg
-              (#5 below) on his adherents would probably be difficult to overestimate. 
-              Instead of confining myself to the older, rhyming poems which usually make up lists like this,
-               I’ve therefore picked and chosen to ensure a fuller range of poets. 
-               This reflects a more diverse canon.
+         My email : anyaribari@gmail.com
             </p>
           </div>
         </div>
